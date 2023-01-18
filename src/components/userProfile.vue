0ce8f03c12f71c3dca382d6c8fc73547425ea936
Anju Shahi <98887057+AnjuShahi21@users.noreply.github.com>
2023-01-18 11:28:37 +0200

    user Profile

--- src/components/userProfile.vue
+++ src/components/userProfile.vue
@@ -1,84 +1,102 @@
 <template>
-    <div>
-        <title>Student Profile - Housekeeper Student Dashboard</title>
-        <body>
-          <userSideBar/>
-
-       
-        <div class="main-content">
+  <div>
+    <title>Student Profile - HostelCleaner Student Dashboard</title>
+    <Sidebar />
+    <div class="main-content">
       <!-- Header -->
       <div class="header bg-background pb-6 pt-5 pt-md-6">
-      <div class="container-fluid">
-       
-      
-      <Header/>
+        <div class="container-fluid">
+
+          <b-alert show variant="success"><span class="alert-link"><strong> Welcome to HostelCleaner's Student
+                portal.</strong></span></b-alert>
+          <Header />
+        </div>
       </div>
-    </div>
-    <!-- Page content -->
-    <div class="container-fluid mt--5">
-      <div class="row mt-2">
-        <div class="col-xl-12 mb-5 mb-xl-0">
-          <div class="card shadow">
-            <div class="card-header border-0">
-              <div class="row align-items-center">
-                <div class="col">
-                  <h5 class="mb-0">Your Profile</h5>
-                </div>
-                <div class="col text-right">
-                  <a href="mailto:anjushahi2002@gmail.com" target="_blank" class="btn btn-sm btn-primary">Request Change</a>
+      <!-- Page content -->
+      <div class="container-fluid mt--5">
+        <div class="row mt-2">
+          <div class="col-xl-12 mb-5 mb-xl-0">
+            <div class="card shadow">
+              <div class="card-header border-0">
+                <div class="row align-items-center">
+                  <div class="col">
+                    <h5 class="mb-0">Student Profile</h5>
+                  </div>
+                  <div class="col text-right">
+                    <a href="mailto:xyz@gmail.com" target="_blank" class="btn btn-sm btn-primary">Request
+                      Change</a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="table-responsive">
-              <!-- Projects table -->
-              <table class="table align-items-center table-flush">
-                <thead class="thead-light">
-                  <tr>
-                    <th scope="col" style="font-size: 12px;">Roll Number</th>
-                    <th scope="col" style="font-size: 13px;"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Hostel Name</th>
-                    <!-- <td></td> -->
-                  </tr>
-                  <tr>
-                    <th scope="row">Floor</th>
-                  
-                  </tr>
-                  <tr>
-                    <th scope="row">Room No.</th>
-                    
-                  </tr>
-                </tbody>
-              </table>
+              <div class="table-responsive">
+                <!-- Projects table -->
+                <table class="table align-items-center table-flush">
+                  <thead class="thead-light">
+                    <tr>
+                      <th scope="col" style="font-size: 12px;">Roll Number</th>
+                      <th scope="col" style="font-size: 13px;">{{ userData.rollNo ? userData.rollNo : 'null' }}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">Hostel Name</th>
+                      <td>{{ userData.hostel ? userData.hostel : 'null' }}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Floor</th>
+                      <td>{{ userData.floor ? userData.floor : 'null' }}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Room No.</th>
+                      <td>
+                        {{ userData.room ? userData.room : 'null' }}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
-  </body>
-    </div>
 
 </template>
 <script>
-import userSideBar from './userSideBar.vue';
+import Sidebar from './Sidebar.vue';
 
 import Header from './Header.vue'
+import { getProfileData } from '@/services/api';
 
-export default{
-    name:'userProfile',
-    userSideBar,
+export default {
+  name: 'userProfile',
+  data() {
+    return { userData: {} }
+  },
+  components: {
+    Sidebar,
     Header
+  },
+  async mounted() {
+    try {
+      const data = await getProfileData();
+      console.log(data);
+      this.userData = data;
+    } catch (e) {
+      console.log(e.message);
+    }
 
-    
+    // localStorage.setItem('userType', JSON.stringify({ type: 'student' }));
+
+  }
+
+
 }
 
 
 </script>
 <style>
-
 
 </style>
