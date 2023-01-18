f592576cdcd6fda15fd32ded384d52c2017eaa1c
Anju Shahi <98887057+AnjuShahi21@users.noreply.github.com>
2023-01-18 11:24:06 +0200

    update register component

--- src/components/RegistrationComp.vue
+++ src/components/RegistrationComp.vue
@@ -1,5 +1,4 @@
 
-
 <template>
   <div>
     <!------ Include the above in your HEAD tag ---------->
@@ -8,83 +7,66 @@
     <div>
       <LoadingOverlay :active="isLoading" />
       <AlertComp :error="error" :success="success" :hideAlert="hideAlert" />
-      <form class="bg bg-white mx-auto my-4 rounded border w-75" @submit.prevent="register">
-        <h3 class="bg bg-primary w-100 px-3 py-2 text text-white">Registration Form</h3>
+      <form class="bg bg-white mx-auto my-4 rounded border " style="max-width: 30rem;" @submit.prevent="register">
+        <h3 class="bg bg-secondary w-100 px-3 py-2 text text-white">Registration Form</h3>
         <div class="p-3">
           <div class="col-sm-12">
-            <div class="row">
-              <div class="col-sm-6 form-group mb-3">
-                <label>Name</label>
-                <input type="text" v-model="formData.name" placeholder="Enter Name Here.." class="form-control">
-              </div>
-              <div class="col-sm-6 form-group mb-3">
-                <label>Email</label>
-                <input type="email" v-model="formData.email" placeholder="Enter Email Here.." class="form-control">
-              </div>
+            <div class="form-group mb-2">
+              <label> Name </label>
+              <input type="text" v-model="formData.name" placeholder="Enter your name.." class="form-control">
             </div>
-            <!-- <div class="row">
-              <div class="col-sm-4 form-group mb-3">
-                <label>City</label>
-                <input type="text" v-model="formData.city" placeholder="Enter City Name Here.." class="form-control">
-              </div>
-              <div class="col-sm-4 form-group mb-3">
-                <label>Pin Code</label>
-                <input type="number" v-model="formData.pincode" placeholder="Enter Pin Code Here.."
-                  class="form-control">
-              </div>
-              <div class="col-sm-4 form-group mb-3">
-                <label>Mobile Number</label>
-                <input type="number" v-model="formData.mob_no" placeholder="Enter Mobile Number Here.."
-                  class="form-control">
-              </div>
-            </div> -->
-            <!-- <div class="row">
-              <div class="col-sm-6 form-group mb-3">
-                <label>Pan Number</label>
-                <input type="text" v-model="formData.pan" placeholder="Enter Pan Number Here.." class="form-control">
-              </div>
-              <div class="col-sm-6 form-group mb-3">
-                <label>Aadhar Number</label>
-                <input type="number" v-model="formData.aadhar" placeholder="Enter Aadhar Number Here.."
-                  class="form-control">
-              </div>
-            </div> -->
-            <!-- <div class="row mb-3">
-              <label>Gender</label>
-                <div class="form-group col-lg-6">
-                  <input class="form-check-input" type="radio" name="gen" v-model="formData.gender" value="Female" aria-label="...">
-                  <label class="form-check-label mx-2" for="flexRadioDefault1">
-                    Female
-                  </label>
-                </div>
-                <div class="form-group col-lg-6">
-                  <input class="form-check-input"  type="radio" name="gen" v-model="formData.gender" value="Male" aria-label="..." checked>
-                  <label class="form-check-label mx-2" for="flexRadioDefault2">
-                    Male
-                  </label>
-                </div>
-            </div> -->
-           
-              
-               
-              <div class="col-sm-6 form-group mb-3">
-                <label>User Type</label>
-                <select v-model="formData.userType" class="form-control">
-                  <option value="student">Student</option>
-                  <option value="admin">Admin</option>
-                </select>
-              </div>
-            
-            <div class="form-group mb-3">
+
+            <div class="form-group mb-2">
+              <label>Email</label>
+              <input type="email" v-model="formData.email" placeholder="Enter your email.." class="form-control">
+            </div>
+
+            <div class="form-group mb-2">
               <label>Password</label>
-              <input type="password" v-model="formData.password" placeholder="Enter Password Here.."
+              <input type="password" v-model="formData.password" placeholder="Enter your password.."
                 class="form-control">
             </div>
+
+
+            <div class="form-group mb-2">
+              <label>Roll Number</label>
+              <input type="number" v-model="formData.rollNo" placeholder="Enter Your Roll No.." class="form-control">
+            </div>
+
+            <div class="form-group mb-2">
+              <label>Hostel</label>
+              <select v-model="formData.hostel" class="form-control">
+                <option value="">-- Select hostel --</option>
+                <option value="pdgh">pdgh</option>
+                <option value="iggh">iggh</option>
+                <option value="rgbh">rgbh</option>
+                <option value="fgp">fgp</option>
+
+              </select>
+            </div>
+
+            <div class="form-group mb-2">
+              <label>Room No.</label>
+              <input type="text" v-model="formData.room" placeholder="Enter your roomno.." class="form-control">
+            </div>
+
+            <div class="form-group mb-2">
+              <label>Floor No</label>
+              <select v-model="formData.floor" class="form-control">
+                <option value="">-- Select Floor --</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
           </div>
-          <div class="my-2">
-            <div class="form-text small"><router-link to="/login">Already a member | Sign In</router-link></div>
+
+          <div class="my-2 mx-4">
+            <div class="form-text small"><router-link to="/login">Already a member | Login</router-link></div>
           </div>
-          <button type="submit" class="btn btn-primary">Register</button>
+          <button type="submit" class="mx-4 my-2 btn btn-secondary">Register</button>
         </div>
       </form>
     </div>
@@ -94,45 +76,48 @@
 <script>
 import AlertComp from '../components/AlertComp.vue';
 import LoadingOverlay from '../components/LoadingOverlay.vue';
-import {register} from '../services/api';
+import { register } from '../services/api';
 export default {
-    name: "RegistrationComp",
-    data() {
-        return {
-            formData: {
-                name: "",
-                email: "",
-                userType: ""
-            },
-            isLoading: false,
-            error: false,
-            success: false
-        };
+  name: "RegistrationComp",
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        rollNo: 0,
+        hostel: "",
+        room: "",
+        floor: "",
+        
+      },
+      isLoading: false,
+      error: false,
+      success: false
+    };
+  },
+  methods: {
+    async register() {
+      console.log(this.formData);
+      this.hideAlert();
+      this.isLoading = true;
+      try {
+        await register(this.formData);
+        this.success = true;
+        console.log("Registered");
+        this.$router.push('/login');
+      }
+      catch (e) {
+        this.error = true;
+        console.log("Error:", e.message);
+      }
+      this.isLoading = false;
     },
-    methods: {
-        async register() {
-            console.log(this.formData);
-            this.hideAlert();
-            this.isLoading = true;
-            try {
-                await register(this.formData);
-                this.success = true;
-                console.log("Registered");
-            }
-            catch (e) {
-                this.error = true;
-                console.log("Error:", e.message);
-            }
-            this.isLoading = false;
-            // localStorage.setItem('isUserExist', true);
-            // this.$router.push('/app');
-        },
-        hideAlert() {
-          this.error = false;
-          this.success = false;
-        }
-    },
-    components: { LoadingOverlay, AlertComp }
+    hideAlert() {
+      this.error = false;
+      this.success = false;
+    }
+  },
+  components: { LoadingOverlay, AlertComp }
 }
 </script>
 
@@ -142,6 +127,7 @@ body {
   padding-top: 20px;
   padding-bottom: 20px;
 }
+
 /* Everything but the jumbotron gets side spacing for mobile first views */
 .header,
 .marketing,
@@ -149,10 +135,12 @@ body {
   padding-right: 15px;
   padding-left: 15px;
 }
+
 /* Custom page header */
 .header {
   border-bottom: 1px solid #e5e5e5;
 }
+
 /* Make the masthead heading the same height as the navigation */
 .header h3 {
   padding-bottom: 19px;
@@ -160,39 +148,48 @@ body {
   margin-bottom: 0;
   line-height: 40px;
 }
+
 /* Custom page footer */
 .footer {
   padding-top: 19px;
   color: #777;
   border-top: 1px solid #e5e5e5;
 }
+
 /* Customize container */
 @media (min-width: 768px) {
   .container {
     max-width: 730px;
   }
 }
+
 .container-narrow>hr {
   margin: 30px 0;
 }
+
 /* Main marketing message and sign up button */
 .jumbotron {
   text-align: center;
   border-bottom: 1px solid #e5e5e5;
 }
+
 .jumbotron .btn {
   padding: 14px 24px;
   font-size: 21px;
 }
+
 /* Supporting marketing content */
 .marketing {
   margin: 40px 0;
 }
+
 .marketing p+h4 {
   margin-top: 28px;
 }
+
 /* Responsive: Portrait tablets and up */
 @media screen and (min-width: 768px) {
+
   /* Remove the padding we set earlier */
   .header,
   .marketing,
@@ -200,10 +197,12 @@ body {
     padding-right: 0;
     padding-left: 0;
   }
+
   /* Space out the masthead */
   .header {
     margin-bottom: 30px;
   }
+
   /* Remove the bottom border on the jumbotron for visual effect */
   .jumbotron {
     border-bottom: 0;
