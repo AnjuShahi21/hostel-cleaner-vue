e55001e356688ac02a15001613c058304beb2348
Anju Shahi <98887057+AnjuShahi21@users.noreply.github.com>
2023-01-18 11:31:16 +0200

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Router from'vue-router';
+import Router from 'vue-router';
 import adDash from '../components/adDash.vue';
-import adComplain from'../components/adComplain.vue';
-import adSuggest from'../components/adSuggest.vue';
-import adStudRegis from'../components/adStudRegis.vue';
-import adWorkerReg from'../components/adWorkerReg.vue';
-import adAllot from'../components/adAllot.vue';
+import adComplain from '../components/adComplain.vue';
+import adSuggest from '../components/adSuggest.vue';
+import adStudRegis from '../components/adStudRegis.vue';
+import adWorkerReg from '../components/adWorkerReg.vue';
+import adAllot from '../components/adAllot.vue';
 import userProfile from '../components/userProfile.vue';
 import userRequest from '../components/userRequest.vue';
 import userFeedback from '../components/userFeedback.vue';
@@ -14,103 +14,112 @@ import RegistrationComp from '../components/RegistrationComp.vue';
 
 function checkaddash(to, from, next) {
     let loginStatus = JSON.parse(localStorage.getItem('token'));
-    if(!loginStatus) {
+    let isAdmin = (loginStatus && (JSON.parse(localStorage.getItem("userType")))['type'] === 'admin');
+    if (!isAdmin) {
         return next('/login');
     }
     next();
 }
-function notLogin(to, from, next) {
+function checkusdash(to, from, next) {
     let loginStatus = JSON.parse(localStorage.getItem('token'));
+    let isStudent = (loginStatus && (JSON.parse(localStorage.getItem("userType")))['type'] === 'student');
 
-    // If loggedIn -> Go to Dashboard
-    if(loginStatus) {
-        return next('/dashboard');
+    if (!isStudent) {
+        return next('/login');
     }
     next();
 }
-function Login(to, from, next){
+function loginCheck(to, from, next) {
     let loginStatus = JSON.parse(localStorage.getItem('token'));
+    let userType = (loginStatus && (JSON.parse(localStorage.getItem("userType")))['type']);
 
     // If loggedIn -> Go to Dashboard
-    if(loginStatus) {
-        return next('/dashboard');
+    if (loginStatus) {
+        return next(userType === 'student' ? '/usdash' : '/addashboard');
     }
     next();
-
 }
 
 
-const router= new Router({
-    routes:[
+const router = new Router({
+    routes: [
         {
-            name:'login',
-            path:'/login',
-            component:LoginComp,
-            beforeEnter:Login
-
-
+            name: 'login',
+            path: '/login',
+            component: LoginComp,
+            beforeEnter: loginCheck
 
         },
         {
-            name:'register',
-            path:'/register',
-            component:RegistrationComp,
-            beforeEnter:notLogin
+            name: 'register',
+            path: '/register',
+            component: RegistrationComp,
+            beforeEnter: loginCheck
 
         },
+        { path: '/', redirect: '/login' },
         {
-            
-            name:'addashboard',
-            path:'/addashboard',
-            component:adDash,
-            beforeEnter:checkaddash
+
+            name: 'addashboard',
+            path: '/addashboard',
+            component: adDash,
+            beforeEnter: checkaddash
         },
         {
-            name:'adallot',
-            path:'/adallot',
-            component:adAllot
+            name: 'adallot',
+            path: '/adallot',
+            component: adAllot,
+            beforeEnter: checkaddash
         },
         {
-            name:'adcomplain',
-            path:'/adcomplain',
-            component:adComplain
+            name: 'adcomplain',
+            path: '/adcomplain',
+            component: adComplain,
+            beforeEnter: checkaddash
         },
         {
-            name:'adsuggest',
-            path:'/adsuggest',
-            component: adSuggest
+            name: 'adsuggest',
+            path: '/adsuggest',
+            component: adSuggest,
+            beforeEnter: checkaddash
         },
         {
-            name:'adstudregis',
-            path:'/adstudregis',
-            component:adStudRegis
+            name: 'adstudregis',
+            path: '/adstudregis',
+            component: adStudRegis,
+            beforeEnter: checkaddash
         },
         {
-            name:'adworkreg',
-            path:'/adworkreg',
-            component:adWorkerReg
+            name: 'adworkreg',
+            path: '/adworkreg',
+            component: adWorkerReg,
+            beforeEnter: checkaddash
         },
         {
-            name:'usprofile',
-            path:'/usprofile',
-            component:userProfile
+            name: 'usprofile',
+            path: '/usprofile',
+            component: userProfile,
+            beforeEnter: checkusdash
         },
         {
-            name:'usrequest',
-            path:'/usrequest',
-            component:userRequest
+            name: 'usrequest',
+            path: '/usrequest',
+            component: userRequest,
+            beforeEnter: checkusdash
         },
         {
-            name:'usfeedback',
-            path:'/usfeedback',
-            component:userFeedback
+            name: 'usfeedback',
+            path: '/usfeedback',
+            component: userFeedback,
+            beforeEnter: checkusdash
         },
         {
-            name:'usdash',
-            path:'/usdash',
-            component:userDash
+            name: 'usdash',
+            path: '/usdash',
+            component: userDash,
+            beforeEnter: checkusdash
         }
-        
+
     ]
 });
 export default router;
