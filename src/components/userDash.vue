c76a2738f891c50423bb5a7e64419308a7aa27bf
Anju Shahi <98887057+AnjuShahi21@users.noreply.github.com>
2023-01-18 11:26:59 +0200

    user dashboard

--- src/components/userDash.vue
+++ src/components/userDash.vue
@@ -1,56 +1,56 @@
 <template>
-    <div>
-        <title>Housekeeper Admin Dashboard</title>
-       
-          <!-- side navigation -->
-        <userSideBar/>
+  <div>
+    <title>HostelCleaner Admin Dashboard</title>
 
-       
-   <!-- Main content -->
+    <!-- side navigation -->
+    <Sidebar />
+
+
+    <!-- Main content -->
     <div class="main-content">
       <!-- Header -->
       <div class="header bg-background pb-6 pt-5 pt-md-6">
-      <div class="container-fluid">
-       
-      
-      <Header/>
-     
+        <div class="container-fluid">
+
+          <b-alert show variant="success"><span class="alert-link"><strong>Welcome to HostelCleaner's Student
+                  portal.</strong></span></b-alert>
+          <Header />
+          </div>
       </div>
-    </div>
-    <!-- Page content -->
-    <div class="container-fluid mt--5">
-      <div class="row mt-2 pb-5">
-        <div class="col-xl-12 mb-5 mb-xl-0">
-          <div class="card shadow">
-            <div class="card-header border-0">
-              <div class="row align-items-center">
-                <div class="col">
-                  <h3 class="mb-0">Housekeeping</h3>
+      <!-- Page content -->
+      <div class="container-fluid mt--5">
+        <div class="row mt-2 pb-5">
+          <div class="col-xl-12 mb-5 mb-xl-0">
+            <div class="card shadow">
+              <div class="card-header border-0">
+                <div class="row align-items-center">
+                  <div class="col">
+                    <h3 class="mb-0 fs-5 ">HostelCleaning</h3>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="table-responsive">
-              <!-- Projects table -->
-              <table class="table align-items-center table-flush">
-                <thead class="thead-light">
-                  <tr>
-                    <th scope="col">Housekeeper</th>
-                   
-                    <th scope="col">Date</th>
-                    <th scope="col">Time Requested</th>
-                    <th scope="col">Time In</th>
-                    <th scope="col">Time Out</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    </tbody>
+              <div class="table-responsive">
+                <!-- Projects table -->
+                <table class="table align-items-center table-flush">
+                  <thead class="thead-light">
+                    <tr>
+                      <th scope="col">HostelCleaner</th>
+
+                      <th scope="col">Date</th>
+                      <th scope="col">Time Requested</th>
+                      <th scope="col">Time In</th>
+                      <th scope="col">Time Out</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
                 </table>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
   </div>
 
@@ -59,23 +59,25 @@
 
 </template>
 <script>
-import userSideBar from './userSideBar.vue'
+import Sidebar from './Sidebar.vue'
 import Header from './Header.vue'
 
-export default{
-    name: "userDashVue",
-    components: {
-      userSideBar,
-      Header
+export default {
+  name: "userDashVue",
+  components: {
+    Sidebar,
+    Header
 
-      
-      }
+
+  },
+  mounted() {
+    localStorage.setItem('userType', JSON.stringify({ type: 'student' }));
+  
+  }
 }
 
 
 </script>
 <style>
-
-
 
 </style>
